7abb9ed97234febcd078e7a0c15ebb6683a4c1e5
ki-bit-web <ki-bit-web@kibit.edu.ua>
2025-01-31 11:04:48 +0200

    ki23-fundamentals programming lesson 3 added if else statement details

--- src/main/java/ua/edu/kibit/Main.java
+++ src/main/java/ua/edu/kibit/Main.java
@@ -2,12 +2,14 @@ package ua.edu.kibit;
 
 public class Main {
 
+    private static int fieldNumber = 7;
+
     public static void main(String[] args) {
         int firstNumber;
         int secondNumber = 4;
         int thirdNumber = 9;
         firstNumber = 8;
-        boolean result = firstNumber < secondNumber;
+//        boolean result = firstNumber < secondNumber;
 
         /*if (firstNumber < secondNumber) {
             if (firstNumber < thirdNumber) {
@@ -49,9 +51,36 @@ public class Main {
         }*/
 
         int i = 1;
+        int temp = factorial;
         while (i <= inputNumber) {
             factorial *= i++;
+            System.out.println(temp);
         }
         System.out.println("factorial=" + factorial);
+
+        /*int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }*/
+
+//        int time = 20;
+//        String result = (time < 18) ? "Good day." : "Good evening.";
+//        System.out.println(result);
+
+        int time = 20;
+
+        if (time < 18) {
+            System.out.println("Good day.");
+        }
+
+        if (time < 18) {
+            System.out.println("Good day.");
+            System.out.println("Other statement if time < 18");
+        }
+
+        boolean booleanResult = (time < 18) ? true : false;
+        booleanResult = time < 18;
     }
 }
