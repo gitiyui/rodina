9b835f938f32aca0a4885508c2a60a5504f3bb72
ki-bit-web <ki-bit-web@kibit.edu.ua>
2025-02-05 11:03:54 +0200

    ki23-fundamentals algo lesson 3 added methods

--- src/main/java/ua/edu/kibit/Main.java
+++ src/main/java/ua/edu/kibit/Main.java
@@ -2,85 +2,65 @@ package ua.edu.kibit;
 
 public class Main {
 
-    private static int fieldNumber = 7;
-
     public static void main(String[] args) {
-        int firstNumber;
-        int secondNumber = 4;
-        int thirdNumber = 9;
-        firstNumber = 8;
-//        boolean result = firstNumber < secondNumber;
+        int min = findMinNumber(8, 4, 9);
+        System.out.println("min=" + min);
+        System.out.println("factorialWithForLoop=" + findFactorialWithForLoop(7));
+        System.out.println("factorialWithReverseForLoop=" + findFactorialWithReverseForLoop(7));
+        System.out.println("factorialWithWhileLoopWithIncrement=" + findFactorialWithWhileLoopWithIncrement(7));
+        System.out.println("factorialWithWhileLoopWithDecrement=" + findFactorialWithWhileLoopWithDecrement(7));
+        System.out.println("factorialWithDoWhileLoopWithDecrement=" + findFactorialWithDoWhileLoopWithDecrement(7));
+    }
 
-        /*if (firstNumber < secondNumber) {
-            if (firstNumber < thirdNumber) {
-                System.out.println("min=firstNumber: " + firstNumber);
-            } else {
-                System.out.println("min=thirdNumber " + thirdNumber);
-            }
-        } else {
-            if (secondNumber < thirdNumber) {
-                System.out.println("min=secondNumber: " + secondNumber);
-            } else {
-                System.out.println("min=thirdNumber " + thirdNumber);
-            }
-        }*/
-
-        /*int min = firstNumber;
+    private static int findMinNumber(int firstNumber, int secondNumber, int thirdNumber) {
+        int min = firstNumber;
         if (secondNumber < min) {
             min = secondNumber;
         }
         if (thirdNumber < min) {
             min = thirdNumber;
         }
-        System.out.println("min=" + min);*/
+        return min;
+    }
 
-        int number = 5;
-        System.out.println(number++);
-        System.out.println(number);
-
-        int inputNumber = 7;
+    private static int findFactorialWithForLoop(int inputNumber) {
         int factorial = 1;
-        /*for (int i = 1; i <= inputNumber; i++) {
+        for (int i = 1; i <= inputNumber; i++) {
             factorial *= i;
         }
-        System.out.println("factorial=" + factorial);*/
+        return factorial;
+    }
 
-        /*for (int i = inputNumber; i >= 1; i--) {
+    private static int findFactorialWithReverseForLoop(int inputNumber) {
+        int factorial = 1;
+        for (int i = inputNumber; i >= 1; i--) {
             factorial *= i;
-            System.out.println("current factorial=" + factorial);
-        }*/
+        }
+        return factorial;
+    }
 
+    private static int findFactorialWithWhileLoopWithIncrement(int inputNumber) {
+        int factorial = 1;
         int i = 1;
-        int temp = factorial;
         while (i <= inputNumber) {
             factorial *= i++;
-            System.out.println(temp);
         }
-        System.out.println("factorial=" + factorial);
+        return factorial;
+    }
 
-        /*int time = 20;
-        if (time < 18) {
-            System.out.println("Good day.");
-        } else {
-            System.out.println("Good evening.");
-        }*/
-
-//        int time = 20;
-//        String result = (time < 18) ? "Good day." : "Good evening.";
-//        System.out.println(result);
-
-        int time = 20;
-
-        if (time < 18) {
-            System.out.println("Good day.");
+    private static int findFactorialWithWhileLoopWithDecrement(int inputNumber) {
+        int factorial = 1;
+        while (inputNumber > 1) {
+            factorial *= inputNumber--;
         }
+        return factorial;
+    }
 
-        if (time < 18) {
-            System.out.println("Good day.");
-            System.out.println("Other statement if time < 18");
-        }
-
-        boolean booleanResult = (time < 18) ? true : false;
-        booleanResult = time < 18;
+    private static int findFactorialWithDoWhileLoopWithDecrement(int inputNumber) {
+        int factorial = 1;
+        do {
+            factorial *= inputNumber--;
+        } while (inputNumber > 1);
+        return factorial;
     }
 }
