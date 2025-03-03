50da509619be7481b0561966959c6c0a0dcf8f3a
gitiyui <rodina.y@kibit.edu.ua>
2025-03-03 13:55:11 +0200

    Added project files

---
+++ src/main/java/ua/edu/kibit/plab1/PerfectNumber.java
@@ -0,0 +1,12 @@
+package main.java.ua.edu.kibit.plab1;
+
+public class PerfectNumber {public static void main(String[] args) {
+    System.out.println(checkPerfectNumber(28));
+    System.out.println(checkPerfectNumber(7));
+}
+
+
+    public static boolean checkPerfectNumber(int num) {
+    return false;
+}
+}
