fb80a6e33f4da5348f867cba8a8f5b8b11a931ff
ki-bit-web <ki-bit-web@kibit.edu.ua>
2025-03-14 15:17:17 +0200

    ki23-fundamentals algo lesson 9 added sortBySelection to ArraySorting

--- src/main/java/ua/edu/kibit/ArraySorting.java
+++ src/main/java/ua/edu/kibit/ArraySorting.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class ArraySorting {
     public static void main(String[] args) {
         int numbers[] = {5, 3, 1, 2, 6, 8, 7};
-        sortByBubble(numbers);
+//        sortByBubble(numbers);
+        sortBySelection(numbers);
     }
 
     private static void sortByBubble(int[] numbers) {
@@ -19,5 +20,33 @@ public class ArraySorting {
             }
         }
         System.out.println(Arrays.toString(numbers));
+    }
+
+    private static void sortBySelection(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+
+            // Assume the current position holds
+            // the minimum element
+            int min_idx = i;
+
+            // Iterate through the unsorted portion
+            // to find the actual minimum
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+
+                    // Update min_idx if a smaller element
+                    // is found
+                    min_idx = j;
+                }
+            }
+
+            // Move minimum element to its
+            // correct position
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
--- src/main/java/ua/edu/kibit/SumMultiples.java
+++ src/main/java/ua/edu/kibit/SumMultiples.java
@@ -2,18 +2,50 @@ package ua.edu.kibit;
 
 public class SumMultiples {
     public static void main(String[] args) {
-        System.out.println(sumOfMultiples(7));
-        System.out.println(sumOfMultiples(10));
-        System.out.println(sumOfMultiples(9));
+//        System.out.println(sumOfMultiples(7));
+//        System.out.println(sumOfMultiples(10));
+//        System.out.println(sumOfMultiples(9));
+        System.out.println(isSumOfMultiplesEqualsNum(9));
     }
 
-    public static int sumOfMultiples(int n) {
+    public static int sumOfMultiples(int num) {
         int sum = 0;
-        for (int i = 3; i <= n; i++) {
+        for (int i = 3; i <= num; i++) {
             if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
                 sum += i;
             }
         }
         return sum;
+    }
+
+    public static boolean isSumOfMultiplesEqualsNum(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 3; i <= num; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                sum += i;
+                if (i != num /i) {
+                    sum = sum * 1;
+                }
+            }
+        }
+        return sum == num;
+    }
+
+    public static boolean isSumOfMultiplesEqualsNum2(int num) {
+        int sum = 0;
+        for (int i = 3; i <= num; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                sum += i;
+                if (sum == num) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
